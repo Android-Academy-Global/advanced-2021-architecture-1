11b8cbfe62dc0c59db431acb0173b7439cbf523d
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2023-02-26 23:50:36 +0300

    Add detekt

---
+++ build-logic/checks/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
